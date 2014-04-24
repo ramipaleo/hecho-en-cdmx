ad7561bdc3960f1cc145fbf3cd1b391dab5a0aae
Antonio Romero <nsdragon@gmail.com>
2014-04-24 16:27:30 -0500

    Enable compass sourcemaps

--- Gruntfile.js
+++ Gruntfile.js
@@ -187,7 +187,7 @@ module.exports = function (grunt) {
         httpFontsPath: '/styles/fonts',
         relativeAssets: false,
         assetCacheBuster: false,
-        raw: 'Sass::Script::Number.precision = 10\n'
+        raw: 'Sass::Script::Number.precision = 10\nsourcemap = true'
       },
       dist: {
         options: {
